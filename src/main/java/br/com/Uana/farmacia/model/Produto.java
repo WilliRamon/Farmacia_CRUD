5a45d9e712669a2c85f494b24b45373f6a19c3fc
WilliRamon <acdc.ramon@hotmail.com>
2022-01-25 13:32:19 -0300

    Adicionando a classe Produto em Model

---
+++ src/main/java/br/com/Uana/farmacia/model/Produto.java
@@ -0,0 +1,78 @@
+package br.com.Uana.farmacia.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotBlank(message = "O atributo Nome não pode ser vazio")
+	@Column(unique = true)
+	@Size(max = 50, message = "O atributo Nome permite no máximo 50 caracteres")
+	private String nome;
+	
+	@NotNull
+	@Size(max = 500, message = "O atributo Descrição permite no máximo 500 caracteres")
+	private String descricao;
+	
+	@Min(0)
+	private double preco;
+	
+	@Min(0)
+	private int qtdEstoque;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public int getQtdEstoque() {
+		return qtdEstoque;
+	}
+
+	public void setQtdEstoque(int qtdEstoque) {
+		this.qtdEstoque = qtdEstoque;
+	}
+	
+	
+}
